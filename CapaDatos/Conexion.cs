57bc02fabb71df6ef86c9ba81c4dbd6867ad7974
Adrian Robledo <122638534+Chacongif@users.noreply.github.com>
2023-01-13 13:15:57 -0600

    Add files via upload

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=DESKTOP-R6TCQDI\\SQLEXPRESS;Initial Catalog=Vanessa;Integrated Security=True";
+    }
+}
